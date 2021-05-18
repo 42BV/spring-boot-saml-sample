734d2efcb433f7c01cd998ae488ae08682e08408
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2021-05-18 10:04:18 +0200

    Showing SAML attributes in UI

--- src/main/java/nl/_42/samldemo/GreetingController.java
+++ src/main/java/nl/_42/samldemo/GreetingController.java
@@ -1,10 +1,17 @@
 package nl._42.samldemo;
 
+import nl._42.boot.saml.user.SAMLAuthentication;
+import nl._42.boot.saml.user.SAMLResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class GreetingController {
@@ -13,6 +20,15 @@ public class GreetingController {
     public String greeting(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("name", authentication.getName());
+
+        Map<String, String> attributes = new HashMap<>();
+        if (authentication instanceof SAMLAuthentication) {
+            SAMLResponse response = ((SAMLAuthentication) authentication).getResponse();
+
+            Collection<String> names = response.getAttributes();
+            names.forEach(name -> attributes.put(name, response.getValues(name).stream().collect(Collectors.joining(", "))));
+        }
+        model.addAttribute("attributes", attributes);
 
         return "greeting";
     }
