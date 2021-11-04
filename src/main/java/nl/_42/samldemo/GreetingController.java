bde06fddc6885d64c384cea4e21370f6326f6506
Jeroen van Schagen <jeroen.van.schagen@42.nl>
2021-11-04 13:52:55 +0100

    Bumped to one login boot 1.0.0 with redirect support

--- src/main/java/nl/_42/samldemo/GreetingController.java
+++ src/main/java/nl/_42/samldemo/GreetingController.java
@@ -1,7 +1,10 @@
 package nl._42.samldemo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import nl._42.boot.onelogin.saml.Saml2Response;
 import nl._42.boot.onelogin.saml.user.Saml2Authentication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,8 +20,10 @@ import java.util.stream.Collectors;
 @Controller
 public class GreetingController {
 
+    private final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
     @GetMapping("/")
-    public String greeting(Model model) {
+    public String greeting(Model model, HttpServletRequest request) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("name", authentication.getName());
         model.addAttribute("authorities", authentication.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", ")));
@@ -26,6 +31,7 @@ public class GreetingController {
         Map<String, String> attributes = new HashMap<>();
         if (authentication instanceof Saml2Authentication) {
             Saml2Response response = ((Saml2Authentication) authentication).getResponse();
+            model.addAttribute("registrationId", response.getRegistrationId());
 
             Collection<String> names = response.getAttributes();
             names.forEach(name -> attributes.put(name, response.getValues(name).stream().collect(Collectors.joining(", "))));
--- src/main/java/nl/_42/samldemo/SamlDemoConfiguration.java
+++ src/main/java/nl/_42/samldemo/SamlDemoConfiguration.java
@@ -1,25 +1,46 @@
 package nl._42.samldemo;
 
+import nl._42.boot.onelogin.saml.web.Saml2Filter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 
 @Configuration
 @EnableWebSecurity
-public class SamlDemoConfiguration extends WebSecurityConfigurerAdapter {
+public class SamlDemoConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, Saml2Filter saml2Filter) throws Exception {
         http
-            .authorizeRequests()
-                .antMatchers("/saml2/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/").permitAll()
-                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
-            .and()
-            .csrf()
-                .ignoringAntMatchers("/saml2/**");
+            .addFilterBefore(saml2Filter, AnonymousAuthenticationFilter.class)
+            .authorizeHttpRequests()
+                .requestMatchers("/saml2/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/").permitAll()
+                .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+            .and().csrf()
+                .ignoringRequestMatchers("/saml2/**")
+            .and().securityContext(securityContext ->
+                securityContext
+                    .requireExplicitSave(true)
+                    .securityContextRepository(securityContextRepository())
+            );
+
+        return http.build();
+    }
+
+    @Bean
+    public DelegatingSecurityContextRepository securityContextRepository() {
+        return new DelegatingSecurityContextRepository(
+            new RequestAttributeSecurityContextRepository(),
+            new HttpSessionSecurityContextRepository()
+        );
     }
 
 }
